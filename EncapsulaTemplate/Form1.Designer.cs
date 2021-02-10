4ec0c16e49e812cad09f2a79e2488fd2b3ac6830
LUIS AUGUSTO FERREIRA <blackbarth@outlook.com>
2021-02-10 08:45:19 -0300

    first commit

---
+++ EncapsulaTemplate/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace EncapsulaTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOrigem = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOrigem
+            // 
+            this.txtOrigem.Location = new System.Drawing.Point(12, 42);
+            this.txtOrigem.Multiline = true;
+            this.txtOrigem.Name = "txtOrigem";
+            this.txtOrigem.Size = new System.Drawing.Size(501, 396);
+            this.txtOrigem.TabIndex = 0;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(570, 42);
+            this.txtDestino.Multiline = true;
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(501, 396);
+            this.txtDestino.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "-->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtOrigem);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerar Codigo Encapsulado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOrigem;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EncapsulaTemplate/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncapsulaTemplate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDestino.Text = string.Empty;
+            if (txtOrigem.Lines.Length > 0)
+            {
+                var aspas = string.Format("\\{0}","\"");
+                var abrechaves = string.Format("\"+\"{0}", "{");
+                var fechachaves = string.Format("\"+\"{0}", "}");
+                for (int i = 0; i < txtOrigem.Lines.Length; i++)
+                {
+                    var linha = "file.AppendLine($\"" + txtOrigem.Lines[i].Replace("\"", aspas).Replace("{", abrechaves).Replace("}", fechachaves) + "\");" + "\r\n";
+                    
+                    txtDestino.AppendText(linha);
+                }
+
+            }
+        }
+    }
+}
